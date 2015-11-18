97d459bd20fbe98f77b217ea2b36bb086546e666
Islam-Mamayeu <Islam_Mamayeu@epam.com>
2015-11-18 18:50:51 +0300

    Update

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.SocketException;
+import java.util.Scanner;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -13,27 +14,20 @@ import org.apache.commons.net.ftp.FTPFile;
 public class Util {
 
 	static FTPClient ftp = new FTPClient();
-	static final private String FtpLogin = "anonymous";
-	static final private String FtpPassword = "";
-
-	public FTPClient getFtp() {
-		return ftp;
-	}
-
-	public void setFtp(FTPClient ftp) {
-		this.ftp = ftp;
-	}
+	static final private String FTPLOGIN = "anonymous";
+	static final private String FTPPASSWORD = "";
 
 	public static boolean connect(String ftpUrl) throws SocketException, IOException {
 		try {
 			ftp.connect(ftpUrl);
 
 			System.out.println("Connected to " + ftpUrl );
-			ftp.login(FtpLogin, FtpPassword);
+			ftp.login(FTPLOGIN, FTPPASSWORD);
 			ftp.enterLocalPassiveMode();
 			// ftp.enterLocalActiveMode();
 
 		} catch (Exception e) {
+			System.out.println("Message: " + e.getMessage());
 			return false;
 
 		}
@@ -47,11 +41,21 @@ public class Util {
         {
 		System.out.println("List of files:");
 		System.out.println("**********************");
-		
-		for (String str : ftp.listNames()) {
-			System.out.println(str);
-
+		FTPFile[] ftpfile = ftp.listFiles();
+		for(FTPFile f : ftpfile)
+		{
+			if(f.isDirectory())
+			{
+				System.out.println("["+f.getName()+"]");	
+			}
+			else
+			{
+				System.out.println(f.getName());
+			}
+			
+			
 		}
+			
 		System.out.println("**********************");
         }
         else
@@ -82,6 +86,105 @@ public class Util {
         }
 
 		
+
+	}
+static public void menu() throws SocketException, IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		boolean b = true;
+
+		System.out.println("Type 'usage' - for more information about command");
+
+		while (b) {
+
+			String choice = sc.nextLine();
+			String[] command = choice.split(" ");
+			if (command[0].equals("connect") && command.length > 1) {
+				if (!(Util.connect(command[1]))) {
+					System.out.println("Connection error");
+
+				}
+			} else {
+
+				switch (command[0]) {
+				//////////////////////////////////////////////////////
+				case "usage":
+
+					System.out.println("connect    -        connection to ftp server (con ftp.example.com)");
+					System.out.println("cd         -        go into folder (cd examle) ");
+					System.out.println("ls         -        to print dir content (print example)");
+					System.out.println("back       -        go to parent dir (parent example) ");
+					System.out.println("download   -        to download file (download example)  ");
+					System.out.println("clear      -        to clear");
+					System.out.println("exit       -        to end");
+					break;
+				//////////////////////////////////////////////////////
+				case "ls":
+					if (Util.ftp.isConnected()) {
+						Util.printFilesName();
+					} else {
+						System.out.println("For a start you have to connect to ftp!");
+					}
+					break;
+
+				//////////////////////////////////////////////////////
+				case "cd":
+
+					if (command.length > 1) {
+
+						if (Util.ftp.changeWorkingDirectory(command[1])) {
+							Util.printFilesName();
+						} else {
+							System.out.println("Incorrect directory");
+						}
+
+					} else {
+						System.out.println("Incorrect command");
+
+					}
+
+					break;
+
+				//////////////////////////////////////////////////////
+				case "back":
+					if (command.length == 1) {
+
+						if (Util.ftp.isConnected()) {
+							Util.ftp.changeToParentDirectory();
+							Util.printFilesName();
+						}
+					} else
+						System.out.println("Incorrect command!");
+
+					break;
+
+				//////////////////////////////////////////////////////
+				case "download":
+                    
+                     if(command.length>1)
+                     {
+					Util.downloadFiles(command[1]);
+                     }
+                     else 
+                     {
+                    	 System.out.println("Incorrect command!");
+                     }
+
+					break;
+				//////////////////////////////////////////////////////
+				case "exit":
+					System.out.println("Good bye");
+					b = false;
+					break;
+				//////////////////////////////////////////////////////
+				default:
+					System.out.println("Incorrect command!");
+					break;
+
+				}
+
+			}
+		}
 
 	}
 	
